7a76715c3dc6433c20ee54c7905b48de82e202ee
totoro <1026215903@qq.com>
2022-06-30 17:22:47 +0800

    居中gogogo

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Main from '@/components/Main'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -8,8 +8,17 @@ export default new Router({
   routes: [
     {
       path: '/',
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: () => import('../components/Login.vue'),
+    },
+    {
+      path: '/main',
       name: 'Main',
-      component: Main
+      component: () => import('../views/Main.vue'),
     }
   ]
 })
---
+++ src/views/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main">
+    <!-- <img src="./assets/shenli.jpg"> -->
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+      msg: 'Welcome 神里大小姐'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
